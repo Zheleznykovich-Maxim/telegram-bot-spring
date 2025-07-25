9f87e930ac4f81a0d5dd05d010d504db628e2c87
Максим Железнякович <m.zheleznyakovich@bft.ru>
2025-07-25 13:16:29 +0300

    Initial commit

---
+++ src/main/java/com/demo/bot/telegram_bot_spring/TelegramBotSpringApplication.java
@@ -0,0 +1,13 @@
+package com.demo.bot.telegram_bot_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotSpringApplication.class, args);
+	}
+
+}
